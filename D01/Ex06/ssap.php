df340d06ce37f054a3ead228a5115ef226a8e2d5
Vaughan SCOTT <vscott@c5r4s2.wethinkcode.co.za>
2019-10-10 08:31:16 +0200

    ssap

---
+++ D01/Ex06/ssap.php
@@ -0,0 +1,14 @@
+#!/usr/bin/php
+<?php
+if($argc > 1)
+{
+	$out = preg_split('/\s+/', ' ', implode(" ", $argv));
+	unset($out[0]);
+	sort($out);
+	$i = 0;
+	while ($out[$i] != NULL)
+	{
+		echo($out[$i++]."\n");
+	}
+}
+?>
