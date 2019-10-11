a671d2c2b13a80e23fa31c1bc0f0a999f9369178
Vaughan SCOTT <vscott@c5r4s2.wethinkcode.co.za>
2019-10-11 14:55:47 +0200

    photos

---
+++ D02/Ex04/photos.php
@@ -0,0 +1,24 @@
+#!/usr/bin/php
+<?php
+    if ($argc != 2)
+        exit();
+	$html = file_get_contents($argv[1]);
+	$dir = parse_url($argv[1], PHP_URL_HOST);
+    if (!file_exists($dir))
+        mkdir($dir);
+	preg_match_all('|<img .*?src=[\'"](.*?)[\'"].*?>|i', $html, $matches);
+	foreach($matches[1] as $link)
+	{
+		$img = substr($link, strrpos($link, "/"));
+		echo("img is".$img."\n");
+		echo("link is".$link."\n");
+        if (!strncmp($link, "http", 4))
+            copy($link, $dir."/".$img);
+        elseif (!strncmp($link, "/", 1))
+            copy($argv[1].$link, $dir."/".$img);
+        elseif (!strncmp($link, "./", 2))
+            copy($argv[1].ltrim($link, '.'), $dir."/".$img);
+        else
+            copy($argv[1]."/".$link, $dir."/".$img);
+    }
+?>
