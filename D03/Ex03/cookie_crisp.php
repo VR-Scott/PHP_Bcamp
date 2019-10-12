3010d500414c891064395357ac9d6d27b00b6f58
Vaughan SCOTT <vscott@c5r4s2.wethinkcode.co.za>
2019-10-12 15:28:53 +0200

    yea

--- D03/Ex03/cookie_crisp.php
+++ D03/Ex03/cookie_crisp.php
@@ -1,12 +1,12 @@
 <?php
-	if ($_GET["action"] == "set" ) 
+	if ($_GET["action"] == "set") 
 	{
-		setcookie($_GET["name"], $_GET["value"], time() + 3600, "/");
-			echo "Cookie: ".$_GET["name"]." has been set to: ".$_GET["value"]."\r\n";
+			setcookie($_GET["name"], $_GET["value"], time() + 3600, "/");
+				echo "Cookie: ".$_GET["name"]." has been set to: ".$_GET["value"]."\r\n";
 	}
 	if ($_GET["action"] == "get")
 		if ((strlen($_COOKIE[$_GET["name"]]) > 0))
-			echo "Cookie: ".$_GET["name"]."'s value is: ".$_COOKIE[$_GET["name"]]."\r\n";
+			echo $_GET["name"]."'s value is: ".$_COOKIE[$_GET["name"]]."\r\n";
 		else
 			echo "Cookie was deleted or does not exist";
 	if ($_GET["action"] == "del")
