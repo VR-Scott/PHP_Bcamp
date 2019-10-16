7fd7b5a2388a2e7a1bfc8121ee8be726c3bc7005
Vaughan SCOTT <vscott@c5r4s2.wethinkcode.co.za>
2019-10-16 17:00:33 +0200

    copied

--- D05/Ex01/ex01.sql
+++ D05/Ex01/ex01.sql
@@ -1,6 +1,6 @@
 CREATE TABLE `ft_table` (
-  `id` int(11) NOT NULL PRIMARY KEY,
-  `login` int(8) NOT NULL,
+  `id` int(11) NOT NULL AUTO_INCREMENT PRIMARY KEY,
+  `login` char(8) DEFAULT 'toto'  NOT NULL,
   `group` enum('staff','student','other') NOT NULL,
   `creation_date` DATE NOT NULL
 );
