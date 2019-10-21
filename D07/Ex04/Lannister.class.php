d533b4fa9b6d74dfd70bb6ce7e85684ad2d11f94
Vaughan SCOTT <vscott@c5r4s2.wethinkcode.co.za>
2019-10-21 14:53:23 +0200

    wowo

---
+++ D07/Ex04/Lannister.class.php
@@ -0,0 +1,9 @@
+<?php
+
+class Lannister
+{
+	public function sleepWith($person)
+	{
+		print($this->with($person) . PHP_EOL);
+	}
+}
---
+++ D07/Ex05/NightsWatch.class.php
@@ -0,0 +1,18 @@
+<?
+	class NightsWatch
+	{
+		public function recruit($person)
+		{
+			if($person instanceof IFighter)
+			{
+				$this->array[] = $person;
+			}
+		}
+		public function fight()
+		{
+			foreach ($this->array as $key => $value)
+			{
+				$value->fight();
+			}
+		}
+	}
